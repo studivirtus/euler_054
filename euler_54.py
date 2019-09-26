431f3e7055fe339e8e4a7d837bda3d52ea8262b3
studivirtus <studivirtus@gmail.com>
2019-09-25 23:09:21 -0500

    Documented work and prepared for sort function

--- euler_54.py
+++ euler_54.py
@@ -4,58 +4,70 @@ handOneList = []
 handTwoList = []
 
 def fileImporter():
+#Used to import the file and prepare it for analysis.
 	csvFile = open("G:\\OneDrive\\code\\project_euler\\euler_054\\p054_poker.txt", "r")
 	csvReader = csv.reader(csvFile, delimiter=" ")
-
-
 	row = 0
 	col = 0
 	for row in csvReader:
+	#for each row prepare each card by seperating the two hands
 		handOne = []
 		handTwo = []
-		counter = 0
+		counter = 0 #for spliting the hands
 		for col in row:
+		#put each card into its the correct hand.
 			if counter < 5:
 				handOne.append(makeList(col))
-				#print ("handOne: ", handOne)
+				#print ("handOne: ", handOne)#DEBUG
 			else:
 				handTwo.append(makeList(col))
-				#print ("handTwo: ", handTwo)
+				#print ("handTwo: ", handTwo)#DEBUG
 			counter += 1
+		#put each properly grouped hand into the correct group of hands.
 		handOneList.append(handOne)
 		handTwoList.append(handTwo)
 	csvFile.close()
 
 	#for row in handOneList:
-		#print (row)
+		#print (row)#DEBUG
 	print("#####################################")
 	#for row in handTwoList:
-		#print (row)
+		#print (row)#DEBUG
 
 def makeList(str):
+#Helper Function: fileImporter
+#Takes a string and creates an indexable list
 	list = []
 	list.append(str[0])
 	list.append(str[1])
 	return list
 
-def isFlish(hand):
+def sortHand(hand):
+	x = 0
+	#start here	
+
+def isFlush(hand):
+#Helper Function: evaluator
+#cycle through the each card in the hand and see if it matches the first card
+#in the hand.
+#Verified
 	suit = ''
 	count = 0
 	proceed = True
 	for col in hand:
-		#print("testpoint_1")
+		#print("testpoint_1")#DEBUG
 		if  count == 0 :
 			suit =  col[1]
-			#print("testpoint_2")
-			#print((suit == col[1]))
+			#print("testpoint_2")#DEBUG
+			#print((suit == col[1]))#DEBUG
 		elif proceed and (suit == col[1]):
-			#print (col[1])
-			#print("testpoint_3")
-			#print(hand)
-			x=0
+			#print (col[1])#DEBUG
+			#print("testpoint_3")#DEBUG
+			#print(hand)#DEBUG
+			x=0 #DEBUG
 		else:
 			proceed = False
-			#print("testpoint_4")
+			#print("testpoint_4")#DEBUG
 		count = count +  1
 		if (count == 5) and (proceed):
 			return True
@@ -63,13 +75,13 @@ def isFlish(hand):
 def evaluator(handOneList, handTwoList):
 	rowNum = 0
 	for row in handOneList:
-		if isFlish(row):
-			print(rowNum)
+		if isFlush(row):
+			print(rowNum) #DEBUG
 		rowNum = rowNum + 1
 	rowNum = 0
 	for row in handTwoList:
-		if isFlish(row):
-			print(rowNum)
+		if isFlush(row):
+			print(rowNum) #DEBUG
 		rowNum = rowNum + 1
 
 #######################################
